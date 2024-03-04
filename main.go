8e9e4deb41dd4c99f249f315f013d31bf489b0cb
A. Anugrah Aqsa <andianugrahaqsa@gmail.com>
2024-03-04 13:20:59 +0800

    revisi assignment 1

--- main.go
+++ main.go
@@ -6,26 +6,65 @@ import (
 	"strconv"
 )
 
-type Friend struct {
+type Student struct {
+	number  int
 	name    string
 	age     int
 	address string
 	reason  string
 }
 
+func printStudent(students []Student, number int) {
+	for _, student := range students {
+		if student.number == number {
+			fmt.Printf("%+v", student)
+			return
+		}
+	}
+	fmt.Printf("%+v", "student not found")
+}
+
 func main() {
+
+	var students = []Student{
+		{
+			number:  1,
+			name:    "Rian",
+			age:     20,
+			address: "Jakarta",
+			reason:  "Ingin fokus belajar Golang",
+		},
+		{
+			number:  2,
+			name:    "Fikri",
+			age:     22,
+			address: "Bandung",
+			reason:  "Memperdalam bahasa Golang",
+		},
+		{
+			number:  3,
+			name:    "Ilham",
+			age:     21,
+			address: "Surabaya",
+			reason:  "Menambah stack di bidang backend",
+		},
+	}
+
 	var argsRaw = os.Args
 
 	var args = argsRaw[1:]
 
-	age, _ := strconv.Atoi(args[1])
-
-	var friend = Friend{
-		name:    args[0],
-		age:     age,
-		address: args[2],
-		reason:  args[3],
+	if len(args) == 0 {
+		fmt.Println("argument is needed")
+		return
 	}
 
-	fmt.Printf("%+v", friend)
+	number, err := strconv.Atoi(args[0])
+
+	if err != nil {
+		fmt.Println("argument must be integer")
+		return
+	}
+
+	printStudent(students, number)
 }
